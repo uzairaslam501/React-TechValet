a33a242d7a2f0c32cea50ae3c3e889b100ca5992
Uzair <ua.aslam501@gmail.com>
2025-01-27 10:08:32 +0000

    earning page done

--- src/pages/Client/Manage/Earnings/Earnings.js
+++ src/pages/Client/Manage/Earnings/Earnings.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   Container,
   Row,
@@ -7,33 +7,124 @@ import {
   Tab,
   Card,
   Button,
-  Table,
+  Spinner,
 } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { getUserEarnings } from "../../../../redux/Actions/serviceActions";
+import {
+  getUserEarnings,
+  getUserEarningRecords,
+} from "../../../../redux/Actions/serviceActions";
+import CustomTable from "../../../../components/Custom/Datatable/table";
+import { useNavigate } from "react-router";
+
+const headers = [
+  { id: "0", label: "Action", column: "Action" },
+  { id: "0", label: "Order #", column: "encOrderId" },
+  {
+    id: "0",
+    label: "Title",
+    column: "orderTitle",
+  },
+  {
+    id: "0",
+    label: "Price",
+    column: "orderPrice",
+  },
+  {
+    id: "0",
+    label: "Earned",
+    column: "earnedFromOrder",
+  },
+  {
+    id: "0",
+    label: "Paid By",
+    column: "orderPaidBy",
+  },
+  {
+    id: "0",
+    label: "Date/Time",
+    column: "completedAt",
+  },
+];
 
 const Earnings = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [records, setRecords] = useState([]);
   const [earnings, setEarnings] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  const [pageLength, setPageLength] = useState(5);
+  const [totalRecord, setTotalRecords] = useState(0);
   const { userAuth } = useSelector((state) => state?.authentication);
+
+  const buttons = [
+    {
+      id: 1,
+      title: "View",
+      onClick: (row) => onView(row),
+      variant: "outline-success",
+      icon: "bi bi-view-stacked",
+    },
+  ];
 
   const getEarnings = async () => {
     dispatch(getUserEarnings(userAuth?.userEncId)).then((response) => {
       setEarnings(response?.payload);
+      getEarningRecords();
     });
+  };
+
+  const getEarningRecords = async (
+    pageNumber = 0,
+    pageLength = 5,
+    sortColumn = "",
+    sortDirection = "",
+    searchParam = "",
+    userId = userAuth?.userEncId
+  ) => {
+    const params = {
+      pageNumber,
+      pageLength,
+      sortColumn,
+      sortDirection,
+      searchParam,
+      userId,
+    };
+
+    try {
+      setIsLoading(true);
+      dispatch(getUserEarningRecords(params))
+        .then((response) => {
+          setRecords(response.payload?.data);
+          setTotalRecords(response.payload?.recordsTotal);
+        })
+        .catch((error) => {
+          setRecords([]);
+          setTotalRecords(0);
+        });
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const stripeAmountWithDraw = async (amount) => {
     console.log("Amount With Draw", amount);
   };
 
+  const onView = (row) => {
+    navigate(`/order-details/${row.encOrderId}`, { state: row });
+  };
+
   useEffect(() => {
-    //getEarnings();
+    setIsLoading(true);
+    getEarnings();
   }, []);
 
   return (
-    <div className="second main-page py-5">
+    <div className="second main-page py-5" style={{ minHeight: "100vh" }}>
       <Container>
         <Row>
           <Col md={6} xs={6} className="mb-3">
@@ -43,7 +134,11 @@ const Earnings = () => {
             <p className="d-flex justify-content-end mb-0 mt-1 p-0 withdrawal-price">
               Available For Stripe Withdrawal:{" "}
               <span className="font-weight-bold text-success">
-                $ {earnings?.balanceAvailable}
+                {!isloading ? (
+                  <>$ {earnings?.balanceAvailable}</>
+                ) : (
+                  <Spinner animation="border" />
+                )}
               </span>
             </p>
           </Col>
@@ -83,27 +178,66 @@ const Earnings = () => {
                       className="font-weight-bold"
                       style={{ fontSize: "13px" }}
                     >
-                      4% of order's amount would be deducted by Stripe on every
-                      order
+                      A 4% fee will be deducted by Stripe from the order amount
+                      for every transaction.
                     </small>
                   </center>
                   <Row className="pt-3">
                     <Col md={4} xs={4} className="text-center border-box">
-                      <p>Total Earning</p>
+                      <p>
+                        Total Earnings{" "}
+                        <sup>
+                          <i
+                            className="bi bi-info bg-secondary text-white rounded-circle"
+                            title="Total earnings are based on the orders you have completed and payments made by clients via Stripe."
+                            style={{
+                              fontSize: "17px",
+                              padding: "1px 2px",
+                            }}
+                          ></i>
+                        </sup>
+                      </p>
                       <h1 className="font-weight-bold m-0">
-                        $ {earnings?.payPalEarning?.stripeTotalBalance}
+                        ${" "}
+                        {earnings?.payPalEarning?.stripeTotalBalance || "0.00"}
                       </h1>
                     </Col>
                     <Col md={4} xs={4} className="text-center border-box">
-                      <p>Completed Order Balance</p>
+                      <p>
+                        Completed Order Balance{" "}
+                        <sup>
+                          <i
+                            className="bi bi-info bg-secondary text-white rounded-circle"
+                            title="This is the balance of the completed orders currently in your Stripe account."
+                            style={{
+                              fontSize: "17px",
+                              padding: "1px 2px",
+                            }}
+                          ></i>
+                        </sup>
+                      </p>
                       <h1 className="font-weight-bold m-0">
-                        $ {earnings?.payPalEarning?.stripeCompletedOrderBalance}
+                        ${" "}
+                        {earnings?.payPalEarning?.stripeCompletedOrderBalance ||
+                          "0.00"}
                       </h1>
                     </Col>
                     <Col md={4} xs={4} className="text-center border-box">
-                      <p>Available Income For WithDraw</p>
+                      <p>
+                        Available Income for Withdrawal{" "}
+                        <sup>
+                          <i
+                            className="bi bi-info bg-secondary text-white rounded-circle"
+                            title="This is the balance of completed orders ready to be transferred to your bank account."
+                            style={{
+                              fontSize: "17px",
+                              padding: "1px 2px",
+                            }}
+                          ></i>
+                        </sup>
+                      </p>
                       <h1 className="font-weight-bold m-0">
-                        $ {earnings?.balanceAvailable}.00
+                        $ {earnings?.balanceAvailable || "0.00"}
                       </h1>
                     </Col>
                   </Row>
@@ -112,16 +246,20 @@ const Earnings = () => {
                   <Button
                     variant="primary-secondary"
                     size="sm"
-                    disabled={earnings?.balanceAvailable === "0" && true}
+                    disabled={
+                      !earnings?.balanceAvailable ||
+                      earnings.balanceAvailable === "0"
+                    }
                     onClick={() =>
                       stripeAmountWithDraw(earnings?.balanceAvailable)
                     }
                   >
                     <i className="bi bi-coin me-1"></i>
-                    {earnings?.balanceAvailable === "0" ? (
-                      <span>Not Enough Payment</span>
+                    {earnings?.balanceAvailable === "0" ||
+                    !earnings?.balanceAvailable ? (
+                      <span>Not Enough Balance</span>
                     ) : (
-                      <span>Withdraw your payment</span>
+                      <span>Withdraw Payment</span>
                     )}
                   </Button>
                 </Card.Footer>
@@ -137,27 +275,70 @@ const Earnings = () => {
                       className="font-weight-bold"
                       style={{ fontSize: "13px" }}
                     >
-                      4% of order's amount would be deducted by PayPal on every
-                      order
+                      A 4% fee will be deducted by PayPal from the order amount
+                      for every transaction.
                     </small>
                   </center>
                   <Row className="pt-3">
+                    {/* Total Earnings */}
                     <Col md={4} xs={4} className="text-center border-box">
-                      <p>Total Earning</p>
+                      <p>
+                        Total Earnings:{" "}
+                        <sup>
+                          <i
+                            className="bi bi-info bg-secondary text-white rounded-circle"
+                            title="Total earnings represent the sum of completed orders and payments received via PayPal."
+                            style={{
+                              fontSize: "17px",
+                              padding: "1px 2px",
+                            }}
+                          ></i>
+                        </sup>
+                      </p>
                       <h1 className="font-weight-bold m-0">
-                        $ {earnings?.payPalEarning?.availableIncomeForWithDrawl}
+                        ${" "}
+                        {earnings?.payPalEarning?.availableIncomeForWithDrawl ||
+                          "0.00"}
                       </h1>
                     </Col>
+
+                    {/* Completed Order Balance */}
                     <Col md={4} xs={4} className="text-center border-box">
-                      <p>Completed Order Balance</p>
+                      <p>
+                        Completed Order Balance:{" "}
+                        <sup>
+                          <i
+                            className="bi bi-info bg-secondary text-white rounded-circle"
+                            title="This balance reflects the total amount you have earned from completed orders through PayPal."
+                            style={{
+                              fontSize: "17px",
+                              padding: "1px 2px",
+                            }}
+                          ></i>
+                        </sup>
+                      </p>
                       <h1 className="font-weight-bold m-0">
-                        $ {earnings?.payPalEarning?.totalEarnedAmount}
+                        $ {earnings?.payPalEarning?.totalEarnedAmount || "0.00"}
                       </h1>
                     </Col>
+
+                    {/* Pending Clearance */}
                     <Col md={4} xs={4} className="text-center border-box">
-                      <p>Pending Clearance</p>
+                      <p>
+                        Pending Clearance:{" "}
+                        <sup>
+                          <i
+                            className="bi bi-info bg-secondary text-white rounded-circle"
+                            title="This represents the amount from completed orders that is still being processed by PayPal and is not yet available for withdrawal."
+                            style={{
+                              fontSize: "17px",
+                              padding: "1px 2px",
+                            }}
+                          ></i>
+                        </sup>
+                      </p>
                       <h1 className="font-weight-bold m-0">
-                        $ {earnings?.payPalEarning?.pendingClearance}
+                        $ {earnings?.payPalEarning?.pendingClearance || "0.00"}
                       </h1>
                     </Col>
                   </Row>
@@ -168,20 +349,19 @@ const Earnings = () => {
         </Tab.Container>
 
         <div className="table-responsive box-table mt-4 bg-white rounded shadow-sm p-2">
-          <Table bordered className="m-0" id="userTable">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>OrderTitle</th>
-                <th>OrderPrice</th>
-                <th>EarnedAmount</th>
-                <th>OrderPaidBy</th>
-                <th>Date/Time</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>{/* Populate rows dynamically */}</tbody>
-          </Table>
+          <h2 className="text-center mt-4">Earning History</h2>
+          <CustomTable
+            headers={headers}
+            records={records}
+            totalRecords={totalRecord}
+            pageLength={pageLength}
+            buttons={buttons}
+            onPageChange={getEarningRecords}
+            onPageLengthChange={setPageLength}
+            loader={isloading}
+            searchFunctionality={false}
+            pageLengthFunctionality={true}
+          />
         </div>
       </Container>
     </div>
